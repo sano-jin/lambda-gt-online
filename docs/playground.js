093160a43d750b5b0279efbbb6d0e1d2a6e3d9a6
sano <sano@ueda.info.waseda.ac.jp>
2022-07-12 23:51:52 +0900

    updated

--- docs/playground.js
+++ docs/playground.js
@@ -92,7 +92,8 @@ case {Log} {nu _Z1. (Cons (_Z1, _Y, _X), Val1 (_Z1))} of
 
 let rec f[_X] nodes[_Y, _X] =
   case {Log} {nodes[_Y, _X]} of
-    {nu _W1. nu _W2. (nodes [_W2, _X], Cons (_W1, _Y, _W2), h [_W1])} -> {f [_X]} { nodes [_Y, _X] }
+    {nu _W1. nu _W2. (nodes [_W2, _X], Cons (_W1, _Y, _W2), h [_W1])}
+      -> {f [_X]} { nodes [_Y, _X] }
     | otherwise -> { Empty }
 in
   {f[_X]}
